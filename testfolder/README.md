5d17c515ca880f552792b7c67379fee32ca4f4da
Your Name <you@example.com>
2017-07-27 13:51:39 +0530

    Added testfolder

---
+++ testfolder/README.md
@@ -0,0 +1,4 @@
+# testfolder
+
+TODO: Enter the cookbook description here.
+
---
+++ testfolder/recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook:: testfolder
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
+
+file '/tmp/srujana' do
+
+      content "Test folder to use the commands with git"
+end
+    
